b15f8bc23c39f7e15ff89c466fe77d94454d3c41
maddox05 <maddoxpublic@gmail.com>
2023-08-01 11:19:06 -0400

    ima stop here till I learn react

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-wordlist = [];
-function getwords(){
+words_you_typed = [];
+words_you_need_to_type = [];
+function typing(){
     const searchvalue = document.getElementById('query');
-    wordlist.push(searchvalue.value)// takes everything from the input field and pushes it to the array
-    console.log(wordlist);
+    words_you_typed.push(searchvalue.value)// takes everything from the input field and pushes it to the array
 }
 function get_words() {
     fetch("https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt")
         .then(response => response.text())
         .then(textData => { // chain of promises
-            console.log(textData);
             parse_words(textData);
         })
         .catch(error => {
@@ -18,6 +17,16 @@ function get_words() {
 function parse_words(text) {
     oneword = [];
     oneword = text.split("\n");
-    console.log(oneword);
+    start(oneword)
 }
+
+function start(oneword) {
+    for(i = 0; i < 100; i++) {
+        words_you_need_to_type[i] = oneword[Math.floor(Math.random()*100)];
+        word_div = document.getElementById('words');
+        word_div.textContent += words_you_need_to_type[i] + " ";
+    }
+}
+
 get_words()
+console.log(words_you_need_to_type)
