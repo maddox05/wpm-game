0d3a261c3312bc49e8bb3a19119110e45a1eeaf7
maddox05 <maddoxpublic@gmail.com>
2023-08-01 21:15:55 -0400

    try 2 later

---
+++ main.js
@@ -0,0 +1,18 @@
+class char {
+    constructor(char, color) {
+        this.char = char;
+        this.color = color;
+    }
+
+}
+function start_game() {
+    // Start the game
+
+}
+function update_game() {
+    // Update the game and start timer
+
+}
+function color_updater() {
+    // Update the color of the char subset of update_game()
+}
